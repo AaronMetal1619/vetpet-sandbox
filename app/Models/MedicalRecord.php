d4d635a747c634707bd1d7a096d1d771408fe41d
Astaroth248 <AaronMetal1619@hotmail.com>
2025-12-02 11:06:41 +0000

    solucionando detalles del cardex nuevo

--- app/Models/MedicalRecord.php
+++ app/Models/MedicalRecord.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class MedicalRecord extends Model
 {
     use HasFactory;
+
+    // FALTABA ESTO: Decirle a Laravel qué campos vamos a usar
+    protected $fillable = [
+        'pet_id',
+        'clinic_name',
+        'visit_date',
+        'diagnosis',
+        'treatment'
+    ];
+
+    // Relación inversa (opcional pero recomendada)
+    public function pet()
+    {
+        return $this->belongsTo(Pet::class);
+    }
 }
